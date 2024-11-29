7a3645a726581aba1f04e94351ddb18b54e5b499
kashaf0007 <166901772+kashaf0007@users.noreply.github.com>
2024-11-29 22:45:17 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+
+
+import Hero from "./component/Hero";
+import Contact from "./component/Contact";
+import Project from "./component/Project";
+import About from "./component/About";
+import Skills from "./component/Skills";
+
+export default function Home() {
+  return (
+    <div>
+    <Hero/>
+    <Contact/>
+    <Project/>
+    <About/>
+    <Skills/>
+      {/* <Navbar /> */}
+    </div>
+  );
+}
